7b930f9e032b493b4b5d1d756a899b2e3778b633
GustavoFossSilva <gustavosilva.nextaage@gmail.com>
2024-03-22 13:57:48 -0300

    posts link

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,9 +1,24 @@
 import { useParams } from "react-router-dom";
+import posts from 'json/posts.json'
+import PostModelo from "componentes/PostModelo";
+import ReactMarkdown from 'react-markdown'
+import './Post.css'
 
 export default function Post() {
     const parametros = useParams();
 
+    const post = posts.find(post => post.id === Number(parametros.id))
+
     return (
-        <h1>{parametros.id}</h1>
+        <PostModelo
+            fotoCapa={`/assets/posts/${post.id}/capa.png`}
+            titulo={post.titulo}
+            >
+            <div className='post-markdown-container'>
+                <ReactMarkdown>    
+                    {post.texto}
+                </ReactMarkdown>
+            </div>
+        </PostModelo>
     );
 }
